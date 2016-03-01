f6b9474b626bbc438a48c6c8f36307a6d5c48b76
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-03-01 22:34:35 +0100

    Added material deisign and a simple layout

--- treater/ember-cli-build.js
+++ treater/ember-cli-build.js
@@ -18,6 +18,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  //
+  app.import('bower_components/material-design-lite/material.min.js');
+  app.import('bower_components/material-design-lite/material.min.css');
 
   return app.toTree();
 };
