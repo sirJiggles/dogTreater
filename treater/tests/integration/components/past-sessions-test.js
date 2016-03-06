bf20ef17c29f5bf56a2f650c31464c45060cd05b
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-03-06 18:06:38 +0100

    Added sauce tree browsers for testing

--- treater/tests/integration/components/past-sessions-test.js
+++ treater/tests/integration/components/past-sessions-test.js
@@ -10,16 +10,16 @@ moduleForComponent('past-sessions', 'Integration | Component | past sessions', {
 test('it shows 3 sessions', function(assert) {
 
   // create a mock of sessions in the past
-  var model = createSessions(3);
+  const MODEL = createSessions(3);
 
   // set the model we mocked
-  this.set('model', model);
+  this.set('model', MODEL);
 
   // render the component with the fake sessions
   this.render(hbs`{{past-sessions sessions=model}}`);
 
   // Get handle to the dom component
-  var $sessions = this.$('.sessions');
+  let $sessions = this.$('.sessions');
 
   assert.equal(
     $sessions.find('.sessions__session').length,
@@ -30,7 +30,7 @@ test('it shows 3 sessions', function(assert) {
   // check the content matches against our model
   assert.equal(
     $sessions.find('.sessions__session__title:eq(0)').text(),
-    model[0].user.name,
+    MODEL[0].user.name,
     'title of first session matches title of first session on model'
   );
 });
